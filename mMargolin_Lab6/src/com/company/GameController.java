a7fa9061fca45d9c19a9bd0bad36b2f4c1c513ab
MartinMargolin <78287232+MartinMargolin@users.noreply.github.com>
2021-02-16 10:56:42 -0700

    Add files via upload

---
+++ mMargolin_Lab6/src/com/company/GameController.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import models.Board;
+
+public class GameController
+{
+    public void run()
+    {
+        char[][] test = new char[7][6];
+        new Board().printBoard(test, "James", "Cunt");
+    }
+
+}
